9b10137b73c1273d75a1b30f13d6d99bcb89819b
sreekanthp <varshisree2@gmail.com>
2025-08-08 19:43:06 +0530

    terraform-aws-eks

--- 80-eks/data.tf
+++ 80-eks/data.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
-    name = "/${var.project}/${var.environment}/private_subnet_ids"
+    name = "/${var.project}/${var.environment}/eks_control_plane_sg_id"
 }
 
 data "aws_ssm_parameter" "eks_node_sg_id" {
